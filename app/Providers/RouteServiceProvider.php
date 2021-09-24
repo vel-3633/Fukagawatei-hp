c5b084a994818097d757111f3979d1d98605a0c8
tokuyasu <fujiiakito3633@gmail.com>
2021-09-24 14:59:47 +0900

    完了3

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,7 +26,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+    protected $namespace = 'App\\Http\\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -61,3 +61,4 @@ class RouteServiceProvider extends ServiceProvider
         });
     }
 }
+
